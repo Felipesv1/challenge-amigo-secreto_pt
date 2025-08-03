efe1a628c42670b40068f4625a87fe9548401175
Felipesv1 <fepereirasilva4@gmail.com>
2025-08-03 16:45:52 -0300

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let friends = [];
+let friendsList = document.getElementById("listaAmigos");
+let sort_friends = document.querySelector(".button-draw");
+let result = document.getElementById("resultado");
+function friendsAdd() {
+  let input_friends = document.getElementById("input_amigo");
+
+  if (input_friends.value == "") {
+    alert("Por favor, insira um nome.");
+  } else {
+    friends.push(input_friends.value);
+    friendsList.innerHTML = friends
+      .map(
+        (item) => `<li>
+        ${item}
+        </li>`
+      )
+      .join("");
+    input_friends.value = "";
+    result.innerHTML = "";
+  }
+}
+
+function friendsSort() {
+  let indice_sort = parseInt(Math.random() * friends.length);
+  result.innerHTML = `<li>
+    ${friends[indice_sort]}
+  </li>`;
+  friendsList.innerHTML = "";
+}
